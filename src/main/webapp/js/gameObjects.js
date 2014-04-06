e2541b6f2376032a67af31cba320b3c971ad5fac
Graham Gatus <graham.gatus@optus.com.au>
2014-04-06 13:20:20 +1000

    Added planes.

--- src/main/webapp/js/gameObjects.js
+++ src/main/webapp/js/gameObjects.js
@@ -11,6 +11,7 @@ AAEmplacement.prototype.updatePosition = function(time) {
 	}
 };
 
+
 AAEmplacement.prototype.fireAABullet = function(coordinate) {
 	var x = coordinate.x;
 	var y = coordinate.y;
@@ -26,11 +27,30 @@ AAEmplacement.prototype.fireAABullet = function(coordinate) {
 	explosions.push(new Explosion({x: x, y: y}, 20));
 };
 
+
+function Base(position) {
+	this.position = position;
+	
+};
+
+Base.prototype.draw = function(context) {
+	context.fillStyle = "brown";
+	context.beginPath();
+	var midpoint = Math.floor(context.canvas.width / 2);
+	context.moveTo(midpoint - 15, 700);
+	context.lineTo(midpoint - 5, 680);
+	context.lineTo(midpoint + 5, 680);
+	context.lineTo(midpoint + 15, 700);
+	context.closePath();
+	context.fill();
+};
+
+
 function Explosion(position, maxRadius) {
 	this.maxRadius = maxRadius;
-	this.spawned = new Date();
 	this.radius = 0;
 	this.alive = true;
+	this.spawned = new Date();
 	this.position = position;
 	this.speed = 50;
 };
@@ -97,7 +117,7 @@ Missile.prototype.updatePosition = function(time) {
 	  	  	
 	  	 context.beginPath();
 	  	 context.lineWidth = 3;
-	  	 context.moveTo(this.startCoord.x, this.startCoord.y)
+	  	 context.moveTo(this.startCoord.x, this.startCoord.y);
 	  	 context.lineTo(this.currentPosition.x, this.currentPosition.y);
 	  	 context.stroke();
 	  	 context.closePath();
@@ -190,6 +210,7 @@ function SmallBullet(start, end) {
 	  	 
 	 	  	context.beginPath();
 	 	  	context.arc(this.position.x, this.position.y, 3, 0, 2 * Math.PI, true);
+	 	  	context.fillStyle = "red";
 	 	  	context.fill();	 
 	 	  	
 	 	  	if (this.position.x == target.x && this.position.y == target.y) {
@@ -205,6 +226,148 @@ SmallBullet.prototype.destroy = function() {
 };
 
 SmallBullet.prototype.isAlive = function() {
+	return this.alive;
+};
+
+
+
+function Emp(startCoord) {
+	  this.spawned = new Date();
+	  this.speed = 700;
+	  this.alive = true;
+	  this.centre = startCoord;
+	  this.radius = 0;
+};
+
+Emp.prototype.updatePosition = function(time) {
+	if (this.alive) {
+		this.radius = this.speed * ((time - this.spawned.getTime()) / 1000);
+		context.beginPath();
+		context.arc(this.startCoord.x, this.startCoord.y, this.radius, 0, 2 * Math.PI, true);
+		context.stroke();
+	}		  
+};
+
+Emp.prototype.hasCollided = function(missile) {
+  	if (this.alive) {
+  		var missilePosition = missile.currentPosition;
+  		var distanceToMissile = Trig.hypot((missilePosition.x - this.centre.x), (missilePosition.y - this.centre.y));
+  		if (distanceToMissile <= this.radius) {
+  			return true;
+  		}
+  	}
+};
+
+function Background() {
+	 this.background = null;
+};
+
+Background.prototype.draw = function(context) {
+	 if (this.background == null) {
+		 this.drawSky(context);
+		 this.drawGround(context);
+		 this.drawSun(context);
+	 	 this.background = context.getImageData(0, 0, context.canvas.width, context.canvas.height);
+	 } else {
+		 context.putImageData(this.background, 0, 0, context.canvas.width, context.canvas.height);
+	 }
+};
+
+Background.prototype.drawSky = function(context) {
+	context.fillStyle = "#0099FF";	//sky blue.
+	context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+};
+
+Background.prototype.drawGround = function(context) {
+	// Fill with a gradient.
+	var gradient = context.createLinearGradient(0, 700, 0, context.canvas.height);
+	gradient.addColorStop(0, '#009900');
+	gradient.addColorStop(0.10, '#336600');
+	gradient.addColorStop(0.50, '#7A5229');
+	gradient.addColorStop(0.75,'#493119');
+	context.fillStyle = gradient;
+	context.fillRect(0, 700, context.canvas.width, context.canvas.height);
+};
+
+Background.prototype.drawSun = function(context) {
+	var rg = context.createRadialGradient (80, 80, 20, 120, 120, 110);
+	rg.addColorStop (0, 'yellow');
+	rg.addColorStop (1, 'red');
+
+	context.fillStyle = rg;
+	context.beginPath();
+	context.arc (120,120,110,0,2*Math.PI,false);
+	context.fill();
+	context.closePath();     	
+};
+
+
+function Jet(position) {
+	this.alive = true;
+	this.speed = 120;
+	this.position = position;
+	this.spawned = new Date();
+	this.lastMoved = new Date();
+};
+
+
+Jet.prototype.draw = function(context) {
+	context.fillStyle = "black";
+	context.lineWidth = 1;
+	context.beginPath();
+	context.moveTo(this.position.x,this.position.y);
+	context.lineTo(this.position.x + 30, this.position.y);
+	context.lineTo(this.position.x + 22, this.position.y - 5);
+	context.lineTo(this.position.x + 40, this.position.y - 15);
+	context.closePath();
+	context.stroke();
+	context.fill();
+};
+
+
+Jet.prototype.updatePosition = function(context, time) {
+	var elapsed = time - this.lastMoved.getTime();
+	this.lastMoved = new Date(time);
+	var distance = this.speed * (elapsed / 1000);
+	this.position.x = this.position.x - distance;
+	
+	this.draw(context);
+	this.fire(context, time);
+};
+
+Jet.prototype.fire = function(context, time) {
+	if (randomInteger(0, 100) > 95) {
+		jetBullets.push(new JetBullet({x: this.position.x + 5, y: this.position.y + 2}));
+	}
+};
+
+function JetBullet(start) {
+	this.verticalAcceleration = 50;
+	this.spawned = new Date();
+	this.position = start;
+	this.velocity = 0;
+	this.alive = true;
+};
+
+JetBullet.prototype.updatePosition = function(context, time) {
+	if (this.position.y > 700) {
+		this.alive = false;
+	} else {
+		var elapsed = time - this.spawned.getTime();
+		var yVelocity = this.verticalAcceleration * (elapsed / 1000);
+		var yDistance = yVelocity * (elapsed / 1000);
+		context.beginPath();
+		context.arc(this.position.x, this.position.y + yDistance, 2, 0, 2 * Math.PI, true);
+		context.fillStyle = "red";
+		context.fill();
+	}
+};
+
+JetBullet.prototype.destroy = function() {
+	this.alive = false;
+};
+
+JetBullet.prototype.isAlive = function() {
 	return this.alive;
 };
 
